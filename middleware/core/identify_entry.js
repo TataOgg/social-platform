8297988e1a35aac9e0dbddb7b8edf894416d29c5
Francisco Hidalgo <lammoth@fjhidalgo-laptop.(none)>
2013-10-15 17:03:21 +0200

    Working on entry identify

---
+++ middleware/core/identify_entry.js
@@ -0,0 +1,22 @@
+
+/**
+ * Save an element
+ */
+
+var settings = require('../../settings');
+
+function createObject(model, subpath) {
+    var modelObject = require(settings.modelsPath + subpath + model);
+
+    return function createObject(req, res, next) {
+        console.log(req.body);
+        modelObject.create(req.body, function(err) {
+            if (err) {
+                return next(err);
+            }
+            next();
+        });
+    };
+}
+
+module.exports = createObject;
